94072b7927b260a128fa3cc8d67c01fc10617aec
saeefShovon <saeef.sheba@gmail.com>
2018-01-17 17:30:07 +0600

    created the application

---
+++ app/src/main/java/com/example/user/contactrecycler/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.user.contactrecycler;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_CONTACTS = 200;
+    RecyclerView rvContacts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS},PERMISSION_CONTACTS);
+        } else {
+            getAllContacts();
+        }
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode){
+            case PERMISSION_CONTACTS:
+                if (grantResults.length >0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                    getAllContacts();
+                }else {
+                    Toast.makeText(this,"DURE GIA MOR",Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    private void getAllContacts() {
+        List<ContactVO> contactVOList = new ArrayList();
+        ContactVO contactVO;
+
+        ContentResolver contentResolver = getContentResolver();
+        Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
+        if (cursor.getCount() > 0) {
+            while (cursor.moveToNext()) {
+
+                int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
+                if (hasPhoneNumber > 0) {
+                    String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                    String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+                    contactVO = new ContactVO();
+                    contactVO.setContactName(name);
+
+                    Cursor phoneCursor = contentResolver.query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                            new String[]{id},
+                            null);
+                    if (phoneCursor.moveToNext()) {
+                        String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        contactVO.setContactNumber(phoneNumber);
+                    }
+
+                    phoneCursor.close();
+
+                    Cursor emailCursor = contentResolver.query(
+                            ContactsContract.CommonDataKinds.Email.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?",
+                            new String[]{id}, null);
+                    while (emailCursor.moveToNext()) {
+                        String emailId = emailCursor.getString(emailCursor.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+                    }
+                    contactVOList.add(contactVO);
+                }
+            }
+
+            AllContactsAdapter contactAdapter = new AllContactsAdapter(contactVOList, getApplicationContext());
+            rvContacts.setLayoutManager(new LinearLayoutManager(this));
+            rvContacts.setAdapter(contactAdapter);
+        }
+    }
+}
